83704c5aacdf6c19e871ab5726119f775a900b89
Luis Carlos Salas Villalobos <lusalas16@gmail.com>
2024-05-08 22:21:40 -0600

    ejemplo

---
+++ example-net-clean-architecture/NCA.Common/NCA.Common.Infrastructure.Log/Logger.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using Serilog;
+using ILogger = NCA.Common.Application.Infrastructure.Log.ILogger;
+
+namespace NCA.Common.Infrastructure.Log
+{
+    public class Logger : ILogger
+    {
+        public Logger()
+        {
+            Serilog.Log.Logger = new LoggerConfiguration()
+                // set default minimum level
+                .MinimumLevel.Information()
+                // add console as target
+                .WriteTo.Console()
+                // add console as target
+                .WriteTo.Debug()
+                .CreateLogger();
+        }
+
+        public void LogVerbose(string message)
+        {
+            Debug.WriteLine(message);
+            Serilog.Log.Verbose(message);
+        }
+
+        public void LogDebug(string message)
+        {
+            Debug.WriteLine(message);
+            Serilog.Log.Debug(message);
+        }
+
+        public void LogInformation(string message)
+        {
+            Debug.WriteLine(message);
+            Serilog.Log.Information(message);
+        }
+
+        public void LogWarning(string message)
+        {
+            Debug.WriteLine(message);
+            Serilog.Log.Warning(message);
+        }
+
+        public void LogError(string message)
+        {
+            Debug.WriteLine(message);
+            Serilog.Log.Error(message);
+        }
+
+        public void LogFatal(string message)
+        {
+            Debug.WriteLine(message);
+            Serilog.Log.Fatal(message);
+        }
+    }
+}
